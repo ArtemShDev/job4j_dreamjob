4ee8269b9f6474de8353221f2eaa761f6ade31fd
ArtemShDev <dev.art.balt@gmail.com>
2021-12-13 11:06:54 +0300

    3. Редактирование вакансии. [#277566]

--- src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
+++ src/main/java/ru/job4j/dream/servlet/CandidateServlet.java
@@ -1,7 +1,6 @@
 package ru.job4j.dream.servlet;
 
 import ru.job4j.dream.model.Candidate;
-import ru.job4j.dream.model.Post;
 import ru.job4j.dream.store.Store;
 
 import javax.servlet.ServletException;
@@ -9,13 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Calendar;
 
 public class CandidateServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        Store.instOf().save(new Candidate(0, req.getParameter("fio")));
+        Store.instOf().save(new Candidate(Integer.parseInt(
+                req.getParameter("id")), req.getParameter("fio")));
         resp.sendRedirect(req.getContextPath() + "/candidates.jsp");
     }
 }
