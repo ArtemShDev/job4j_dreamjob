c13bb90a8053ce3ecba3718225c91dc146075c3a
ArtemShDev <dev.art.balt@gmail.com>
2021-12-15 13:05:03 +0300

    3. Тесты на DbStore [#504817 #240028]

--- src/test/java/ru/job4j/dream/store/DbStoreTest.java
+++ src/test/java/ru/job4j/dream/store/DbStoreTest.java
@@ -48,12 +48,15 @@ public class DbStoreTest {
             store.deletePost(post.getId());
         }
         Collection<Post> posts = new ArrayList<>();
-        posts.add(new Post(0, "Java Job 1"));
-        posts.add(new Post(0, "Java Job 2"));
-        posts.add(new Post(0, "Java Job 3"));
-        for (Post post : posts) {
-            store.save(post);
-        }
+        Post post1 = new Post(0, "Java Job 1");
+        Post post2 = new Post(0, "Java Job 2");
+        Post post3 = new Post(0, "Java Job 3");
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post3);
+        store.save(post1);
+        store.save(post2);
+        store.save(post3);
         assertThat(store.findAllPosts(), is(posts));
     }
 
@@ -93,12 +96,15 @@ public class DbStoreTest {
             store.deleteCandidate(candidate.getId());
         }
         Collection<Candidate> candidates = new ArrayList<>();
-        candidates.add(new Candidate(0, "New Candidate - Java Junior 1"));
-        candidates.add(new Candidate(0, "New Candidate - Java Junior 2"));
-        candidates.add(new Candidate(0, "New Candidate - Java Junior 3"));
-        for (Candidate candidate : candidates) {
-            store.save(candidate);
-        }
+        Candidate candidate1 = new Candidate(0, "New Candidate - Java Junior 1");
+        Candidate candidate2 = new Candidate(0, "New Candidate - Java Junior 2");
+        Candidate candidate3 = new Candidate(0, "New Candidate - Java Junior 3");
+        candidates.add(candidate1);
+        candidates.add(candidate2);
+        candidates.add(candidate3);
+        store.save(candidate1);
+        store.save(candidate2);
+        store.save(candidate3);
         assertThat(store.findAllCandidates(), is(candidates));
     }
 }
